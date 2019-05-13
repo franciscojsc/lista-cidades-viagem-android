e1a78b0a91e9251825558db7bafe04cc96039c7b
Francisco Chaves <francisco.jschaves@gmail.com>
2019-05-13 16:19:54 -0300

    Adicionando cidades para exibir na lista

--- app/src/main/java/br/com/franciscochaves/cidadesparaviagem/MainActivity.java
+++ app/src/main/java/br/com/franciscochaves/cidadesparaviagem/MainActivity.java
@@ -2,12 +2,46 @@ package br.com.franciscochaves.cidadesparaviagem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListaItens;
+    private String[] itens = {
+            "Angra dos Reis",
+            "Cladas novas",
+            "Aracaju",
+            "Campos do Jordão",
+            "Ilhéus",
+            "Porto Seguro",
+            "Costa do Sauípe",
+            "Rio de Janeiro",
+            "Santiago",
+            "Praga",
+            "Buenos Aires",
+            "Budapest",
+            "Cancun",
+            "Aruba",
+            "Caribe",
+            "Tiradentes",
+            "Zurique"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListaItens = findViewById(R.id.list_cidades);
+
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                itens);
+
+        mListaItens.setAdapter(adaptador);
+
     }
 }
